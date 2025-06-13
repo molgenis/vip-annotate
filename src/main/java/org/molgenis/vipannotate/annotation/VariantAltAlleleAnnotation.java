1692d434eb8bf820b50d06c59f103f77744ada61
dennishendriksen <d.hendriksen@umcg.nl>
2025-06-13 19:48:41 +0200

    refactor: wip

--- src/main/java/org/molgenis/vipannotate/annotation/VariantAltAlleleAnnotation.java
+++
@@ -1,9 +0,0 @@
-package org.molgenis.vipannotate.annotation;
-
-/**
- * Alternate non-reference allele annotations
- *
- * @param variant alternate non-reference allele
- * @param variantAnnotations annotations
- */
-public record VariantAltAlleleAnnotation<T>(Variant variant, T variantAnnotations) {}
