2ea0a3b52d3a88fd69ad5981b92e8ee66e899725
dennishendriksen <d.hendriksen@umcg.nl>
2025-06-13 20:21:02 +0200

    refactor: wip

--- src/main/java/org/molgenis/vipannotate/annotation/VcfAnnotatorFactory.java
+++ src/main/java/org/molgenis/vipannotate/annotation/VcfAnnotatorFactory.java
@@ -4,6 +4,9 @@ import java.nio.file.Path;
 import java.util.List;
 import lombok.NonNull;
 import org.molgenis.vipannotate.annotation.gnomad.GnomAdAnnotatorFactory;
+import org.molgenis.vipannotate.annotation.ncer.NcERAnnotatorFactory;
+import org.molgenis.vipannotate.annotation.phylop.PhyloPAnnotatorFactory;
+import org.molgenis.vipannotate.annotation.remm.RemmAnnotatorFactory;
 import org.molgenis.vipannotate.vcf.*;
 
 public class VcfAnnotatorFactory implements AutoCloseable {
@@ -28,15 +31,15 @@ public class VcfAnnotatorFactory implements AutoCloseable {
   private VcfRecordAnnotatorAggregator createVcfRecordAnnotator(Path annotationsDir) {
     VcfRecordAnnotator vcfRecordAnnotatorGnomAd =
         new GnomAdAnnotatorFactory(annotationBlobReaderFactory).create(annotationsDir);
-    // FIXME enable annotators
-    //    VcfRecordAnnotator vcfRecordAnnotatorPhyloP =
-    // PhyloPAnnotatorFactory.create(annotationsDir);
-    //    VcfRecordAnnotator vcfRecordAnnotatorNcER = NcERAnnotatorFactory.create(annotationsDir);
-    //    VcfRecordAnnotator vcfRecordAnnotatorRemm = RemmAnnotatorFactory.create(annotationsDir);
-    return new VcfRecordAnnotatorAggregator(List.of(vcfRecordAnnotatorGnomAd /*,
+    VcfRecordAnnotator vcfRecordAnnotatorNcER = NcERAnnotatorFactory.create(annotationsDir);
+    VcfRecordAnnotator vcfRecordAnnotatorPhyloP = PhyloPAnnotatorFactory.create(annotationsDir);
+    VcfRecordAnnotator vcfRecordAnnotatorRemm = RemmAnnotatorFactory.create(annotationsDir);
+    return new VcfRecordAnnotatorAggregator(
+        List.of(
+            vcfRecordAnnotatorGnomAd,
             vcfRecordAnnotatorNcER,
             vcfRecordAnnotatorPhyloP,
-            vcfRecordAnnotatorRemm*/));
+            vcfRecordAnnotatorRemm));
   }
 
   @Override
