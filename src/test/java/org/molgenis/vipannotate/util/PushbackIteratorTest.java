b2e781e88deee5024ce5d5f1840ac6019845945d
dennishendriksen <d.hendriksen@umcg.nl>
2025-06-17 09:28:54 +0200

    refactor: add unit tests

---
+++ src/test/java/org/molgenis/vipannotate/util/PushbackIteratorTest.java
@@ -0,0 +1,63 @@
+package org.molgenis.vipannotate.util;
+
+import static java.util.Collections.emptyIterator;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+
+class PushbackIteratorTest {
+  @Test
+  void newWithNullIterator() {
+    //noinspection DataFlowIssue
+    assertThrows(NullPointerException.class, () -> new PushbackIterator<Integer>(null));
+  }
+
+  @Test
+  void hasNextNext() {
+    Iterator<Integer> integerIt = List.of(1, 2, 3).iterator();
+    PushbackIterator<Integer> it = new PushbackIterator<>(integerIt);
+    assertTrue(it.hasNext());
+    assertEquals(1, it.next());
+    assertEquals(2, it.next());
+    assertEquals(3, it.next());
+    assertFalse(it.hasNext());
+  }
+
+  @Test
+  void pushback() {
+    Iterator<Integer> integerIt = List.of(1, 2).iterator();
+    PushbackIterator<Integer> it = new PushbackIterator<>(integerIt);
+    assertTrue(it.hasNext());
+
+    Integer pushbackElement = it.next();
+    assertEquals(1, pushbackElement);
+    it.pushback(pushbackElement);
+    assertEquals(1, it.next());
+    assertEquals(2, it.next());
+    assertFalse(it.hasNext());
+  }
+
+  @Test
+  void pushbackLast() {
+    Iterator<Integer> integerIt = List.of(1, 2).iterator();
+    PushbackIterator<Integer> it = new PushbackIterator<>(integerIt);
+    assertTrue(it.hasNext());
+    assertEquals(1, it.next());
+    Integer pushbackElement = it.next();
+    assertEquals(2, pushbackElement);
+    assertFalse(it.hasNext());
+    it.pushback(pushbackElement);
+    assertTrue(it.hasNext());
+    assertEquals(2, it.next());
+    assertFalse(it.hasNext());
+  }
+
+  @Test
+  void nextNotSuchElementException() {
+    Iterator<String> it = new PushbackIterator<>(emptyIterator());
+    assertThrows(NoSuchElementException.class, it::next);
+  }
+}
