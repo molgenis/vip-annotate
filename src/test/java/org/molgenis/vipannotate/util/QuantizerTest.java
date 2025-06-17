06985fe42df1f228d1d12e87807a186f03c7af29
dennishendriksen <d.hendriksen@umcg.nl>
2025-06-17 09:19:27 +0200

    refactor: add unit tests

--- src/test/java/org/molgenis/vipannotate/util/QuantizerTest.java
+++ src/test/java/org/molgenis/vipannotate/util/QuantizerTest.java
@@ -3,6 +3,7 @@ package org.molgenis.vipannotate.util;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,9 +18,29 @@ class QuantizerTest {
         Arguments.of(0d, 1d, 1, (1 << 16) - 1));
   }
 
+  @Test
+  void quantizeInvalidSourceInterval() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.quantize(1d, 1d, 0d, 0, 1));
+  }
+
+  @Test
+  void quantizeInvalidTargetInterval() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.quantize(1d, 0d, 1d, 1, 0));
+  }
+
+  @Test
+  void quantizeInvalidValueOutsideOfRangeLeft() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.quantize(-1d, 0d, 1d, 0, 1));
+  }
+
+  @Test
+  void quantizeInvalidValueOutsideOfRangeRight() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.quantize(2d, 0d, 1d, 0, 1));
+  }
+
   @ParameterizedTest
   @MethodSource("quantizeAndDequantizeProvider")
-  public void quantizeAndDequantize(double x, double y, int u, int v) {
+  void quantizeAndDequantize(double x, double y, int u, int v) {
     double maxQuantizationError = (y - x) / (2 * (v - u));
     double totalQuantizationError = 0d;
 
@@ -38,12 +59,32 @@ class QuantizerTest {
 
   @ParameterizedTest
   @MethodSource("quantizeAndDequantizeProvider")
-  public void quantizeAndDequantizeRightClosed(double x, double yAndValue, int u, int v) {
+  void quantizeAndDequantizeRightClosed(double x, double yAndValue, int u, int v) {
     double maxQuantizationError = 1d / (2 * (v - u));
 
     int quantizedValue = Quantizer.quantize(yAndValue, x, yAndValue, u, v);
     double dequantizedValue = Quantizer.dequantize(quantizedValue, u, v, x, yAndValue);
 
     assertEquals(yAndValue, dequantizedValue, maxQuantizationError);
+  }
+
+  @Test
+  void dequantizeInvalidSourceInterval() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.dequantize(1, 0, 1, 1d, 0d));
+  }
+
+  @Test
+  void dequantizeInvalidTargetInterval() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.dequantize(1, 1, 0, 0d, 1d));
+  }
+
+  @Test
+  void dequantizeInvalidValueOutsideOfRangeLeft() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.dequantize(-1, 0, 1, 0d, 1d));
+  }
+
+  @Test
+  void dequantizeInvalidValueOutsideOfRangeRight() {
+    assertThrows(IllegalArgumentException.class, () -> Quantizer.dequantize(2, 0, 1, 0d, 1d));
   }
 }
