c3ae4f8553912a61f441ad94dcec73633e464b44
dennishendriksen <d.hendriksen@umcg.nl>
2025-08-26 07:37:12 +0200

    wip: remove unused SortedLongArrayWrapper

--- src/main/java/org/molgenis/vipannotate/serialization/SortedLongArrayWrapper.java
+++
@@ -1,6 +0,0 @@
-package org.molgenis.vipannotate.serialization;
-
-import java.io.Serializable;
-
-/** sorted long[] wrapper with custom {@link SortedLongArrayWrapperSerializer} */
-public record SortedLongArrayWrapper(long[] array) implements Serializable {}
