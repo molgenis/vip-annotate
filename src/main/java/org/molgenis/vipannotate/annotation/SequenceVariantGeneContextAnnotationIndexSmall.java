122ce5d7bbdbb9ba8621b7c7fc520ba9a06537fc
dennishendriksen <d.hendriksen@umcg.nl>
2025-08-26 08:14:43 +0200

    fix: SequenceVariantGeneContextAnnotationIndexSmall should use SortedLongArrayWrapper instead of SortedIntArrayWrapper

--- src/main/java/org/molgenis/vipannotate/annotation/SequenceVariantGeneContextAnnotationIndexSmall.java
+++ src/main/java/org/molgenis/vipannotate/annotation/SequenceVariantGeneContextAnnotationIndexSmall.java
@@ -2,9 +2,9 @@ package org.molgenis.vipannotate.annotation;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import org.molgenis.vipannotate.serialization.SortedIntArrayWrapper;
+import org.molgenis.vipannotate.serialization.SortedLongArrayWrapper;
 
-public record SequenceVariantGeneContextAnnotationIndexSmall(SortedIntArrayWrapper encodedVariants)
+public record SequenceVariantGeneContextAnnotationIndexSmall(SortedLongArrayWrapper encodedVariants)
     implements Serializable {
   public int findIndex(SequenceVariantGeneContext variant) {
     long encodedSmallVariant = SequenceVariantGeneContextEncoder.encodeSmall(variant);
