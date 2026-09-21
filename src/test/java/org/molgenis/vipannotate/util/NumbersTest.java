a6858ba90d2c09cdfd03dd330a93f02681e568d8
dennishendriksen <d.hendriksen@umcg.nl>
2026-09-21 21:36:19 +0200

    test: NumbersTest safeLongToShort* safeLongToInt*

--- src/test/java/org/molgenis/vipannotate/util/NumbersTest.java
+++ src/test/java/org/molgenis/vipannotate/util/NumbersTest.java
@@ -3,6 +3,8 @@ package org.molgenis.vipannotate.util;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class NumbersTest {
   // int positive
@@ -201,19 +203,76 @@ class NumbersTest {
     assertThrows(IllegalArgumentException.class, () -> Numbers.validateUnitInterval(1.1d));
   }
 
-  @Test
-  void safeIntToByte() {
-    assertEquals((byte) 64, Numbers.safeLongToByte(64));
+  @ParameterizedTest
+  @ValueSource(longs = {Byte.MIN_VALUE, 0L, Byte.MAX_VALUE})
+  void safeLongToByte(long value) {
+    assertEquals((byte) value, Numbers.safeLongToByte(value));
   }
 
-  @Test
-  void safeIntToByteTooLow() {
-    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToByte(-256));
+  @ParameterizedTest
+  @ValueSource(longs = {Byte.MIN_VALUE - 1L, Byte.MAX_VALUE + 1L})
+  void safeLongToByteOutOfRange(long value) {
+    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToByte(value));
   }
 
-  @Test
-  void safeIntToByteTooHigh() {
-    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToByte(256));
+  @ParameterizedTest
+  @ValueSource(longs = {0L, 0xFFL})
+  void safeLongToUnsignedByte(long value) {
+    assertEquals((byte) value, Numbers.safeLongToUnsignedByte(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {-1L, 256L})
+  void safeLongToUnsignedByteOutOfRange(long value) {
+    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToUnsignedByte(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {Short.MIN_VALUE, 0L, Short.MAX_VALUE})
+  void safeLongToShort(long value) {
+    assertEquals((short) value, Numbers.safeLongToShort(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {Short.MIN_VALUE - 1L, Short.MAX_VALUE + 1L})
+  void safeLongToShortOutOfRange(long value) {
+    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToShort(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {0L, 0xFFFFL})
+  void safeLongToUnsignedShort(long value) {
+    assertEquals((short) value, Numbers.safeLongToUnsignedShort(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {-1L, 65536L})
+  void safeLongToUnsignedShortOutOfRange(long value) {
+    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToUnsignedShort(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {Integer.MIN_VALUE, 0L, Integer.MAX_VALUE})
+  void safeLongToInt(long value) {
+    assertEquals((int) value, Numbers.safeLongToInt(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {Integer.MIN_VALUE - 1L, Integer.MAX_VALUE + 1L})
+  void safeLongToIntOutOfRange(long value) {
+    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToInt(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {0L, 0xFFFFFFFFL})
+  void safeLongToUnsignedInt(long value) {
+    assertEquals((int) value, Numbers.safeLongToUnsignedInt(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(longs = {-1L, 4294967296L})
+  void safeLongToUnsignedIntOutOfRange(long value) {
+    assertThrows(IllegalArgumentException.class, () -> Numbers.safeLongToUnsignedInt(value));
   }
 
   @Test
